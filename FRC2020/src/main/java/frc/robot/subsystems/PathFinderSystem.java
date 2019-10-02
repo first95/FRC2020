9bfb15f2ecb0c5b480b3b62d6e05cca0b075ebad
PJG3000\Philip <philip@sugargroup.net>
2019-10-02 19:17:35 -0400

    Added the basics (subsystem and command) to establish the branch.

---
+++ FRC2020/src/main/java/frc/robot/subsystems/PathFinderSystem.java
@@ -0,0 +1,29 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.commands.drivebase.PathFinderCommand;
+
+/**
+ * Add your docs here.
+ */
+public class PathFinderSystem extends Subsystem
+{
+  public void pathFinder(boolean realHardware)
+  {
+    
+  }
+
+
+  @Override
+  protected void initDefaultCommand()
+  {
+    setDefaultCommand(new PathFinderCommand());
+  }
+}
