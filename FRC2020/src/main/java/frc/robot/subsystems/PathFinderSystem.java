8fc375a857773701639bbdbc08c0e3756b25db89
PJG3000\Philip <philip@sugargroup.net>
2019-10-23 18:15:32 -0400

    Added some example code from the wiki to try and understand how to use this code.

--- FRC2020/src/main/java/frc/robot/subsystems/PathFinderSystem.java
+++ FRC2020/src/main/java/frc/robot/subsystems/PathFinderSystem.java
@@ -9,15 +9,33 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.commands.drivebase.PathFinderCommand;
+import jaci.pathfinder.Pathfinder;
+import jaci.pathfinder.Trajectory;
+import jaci.pathfinder.Waypoint;
+import jaci.pathfinder.modifiers.TankModifier;
 
 /**
  * Add your docs here.
  */
 public class PathFinderSystem extends Subsystem
 {
-  public void pathFinder(boolean realHardware)
+  public void pathFinder()
   {
-    
+    Waypoint[] points = new Waypoint[] {
+      new Waypoint(-4, -1, Pathfinder.d2r(-45)),      // Waypoint @ x=-4, y=-1, exit angle=-45 degrees
+      new Waypoint(-2, -2, 0),                        // Waypoint @ x=-2, y=-2, exit angle=0 radians
+      new Waypoint(0, 0, 0)                           // Waypoint @ x=0, y=0,   exit angle=0 radians
+  };
+  
+  Trajectory.Config config = new Trajectory.Config(Trajectory.FitMethod.HERMITE_CUBIC, Trajectory.Config.SAMPLES_HIGH, 0.05, 1.7, 2.0, 60.0);
+  Trajectory trajectory = Pathfinder.generate(points, config);
+
+        // Wheelbase Width = 0.5m
+        TankModifier modifier = new TankModifier(trajectory).modify(0.5);
+
+        // Do something with the new Trajectories...
+        Trajectory left = modifier.getLeftTrajectory();
+        Trajectory right = modifier.getRightTrajectory();
   }
 
 
