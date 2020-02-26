6a4025d44ed876c6c943faad845b93cedd99b761
Shared01 <someone@example.com>
2020-02-26 18:51:24 -0500

    Starting to add shooter control that is simple state-based (not fully open nor closed); main step here is adding use of built-in encoder to get the speed and put in SmartDashboard so we know we can use that.

--- FRC2020/src/main/java/frc/robot/commands/AutoPowerCellMover.java
+++ FRC2020/src/main/java/frc/robot/commands/AutoPowerCellMover.java
@@ -35,6 +35,10 @@ public class AutoPowerCellMover extends Command {
   public static double SINGULATOR_RUN_SPEED = 0.5;
 
   public static double MANUAL_RUN_SPEED_SHOOTER = 0.5;
+  public static double TARGET_RUN_SPEED_SHOOTER = 3000; // ideal speed in RPM
+  public static double RUN_TOLERANCE_SHOOTER = 500; // tolerance range for shooter speed
+  public static double MAINTAIN_RUN_SPEED_SHOOTER = 0.4; // want this to roughly hold target RPM
+  public static double SLOW_INC_SPEED_SHOOTER = 0.1; // amount to slow down shooter by when running too fast
   public static double MANUAL_REDUCTION = 0.2;
   public static double MIN_RUN_SPEED = 0.05;
   private double current_speed = 0;
