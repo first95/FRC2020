4695ba684650a84b325ef0e3664ef27cd7fc3736
zaaj <adam.a.johnson@gmail.com>
2020-02-28 12:18:14 -0500

    shut off wheels after auto drive

--- FRC2020/src/main/java/frc/robot/commands/autocommands/AutoDrive.java
+++ FRC2020/src/main/java/frc/robot/commands/autocommands/AutoDrive.java
@@ -46,6 +46,7 @@ public class AutoDrive extends Command {
   // Called once after isFinished returns true
   @Override
   protected void end() {
+    Robot.drivebase.driveWithTankControls(0, 0);
   }
 
   // Called when another command which requires one or more of the same
